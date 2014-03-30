f9efc1a1986addc9049031b796c4c7608c08b0f5
Kaltsoon <Kalle.ilves@helsinki.fi>
2014-03-30 09:23:00 +0300

    virhe korjattu reference modelissa

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -3,12 +3,12 @@ class Reference < ActiveRecord::Base
 	validates :name, uniqueness: true, presence: true
 	validates :ref_type, presence: true
 
-	def has_required_attribute(attribute)
+	def has_required_attribute?(attribute)
 		return "#{ref_type.downcase.capitalize}Reference".constantize.required_attributes.include?(attribute.name)
 	end
 
 	def get_available_attributes()
-		return "#{ref_type.downcase.capitalize}Reference".constantize.required_attributes
+		return "#{ref_type.downcase.capitalize}Reference".constantize.available_attributes
 	end
 
 	def get_attributes_not_set()
