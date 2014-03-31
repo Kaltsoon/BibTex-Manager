a1150b4509fd272ab0401f299154e52aa14d0174
Hicks48 <henri.korpela@hotmail.com>
2014-03-31 10:47:34 +0300

    BibTex generator

---
+++ lib/bibtex_generator.rb
@@ -0,0 +1,24 @@
+module BibTexGenerator
+
+	def generate_bibtex_string(references)
+		bibtex_file = ""
+		references.each do |reference|
+			bibtex_file += generate_bibtex_for_reference(reference) + "\n"
+		end
+		return bibtex_file
+	end
+	
+	private
+	
+	def generate_bibtex_for_reference(reference)
+		bibtex = "@" + reference[:ref_type] + "{" + reference[:name] + ",\n"
+		i = 0
+		reference[:reference_attributes].each do |ref_att|
+			bibtex += ref_att[:name] + " = " + "{" + ref_att[:value] + "}"+ (i<(reference[:reference_attributes].count-1) ? "," : "") + "\n"
+			i += 1
+		end
+		bibtex += "}"
+		return bibtex
+	end
+
+end
