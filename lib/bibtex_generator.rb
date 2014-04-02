914ac3584e591929d15b3beb4bebb8a039a9b958
Hicks48 <henri.korpela@hotmail.com>
2014-04-02 10:03:31 +0300

    Lisaysksiagit status! :D

--- lib/bibtex_generator.rb
+++ lib/bibtex_generator.rb
@@ -1,6 +1,10 @@
+#encoding: utf-8
+
 module BibtexGenerator
 	extend ActiveSupport::Concern
-
+	
+	special_symbols = {'ä' => '\\"{a}', 'ö' => '\\"{o}'}
+	
 	def generate_bibtex_string(references)
 		bibtex_file = ""
 		references.each do |reference|
@@ -8,6 +12,8 @@ module BibtexGenerator
 		end
 		return bibtex_file
 	end
+	
+	private
 	
 	def generate_bibtex_for_reference(reference)
 		bibtex = "@" + reference.ref_type + "{" + reference.name + ",\n"
@@ -19,5 +25,25 @@ module BibtexGenerator
 		bibtex += "}"
 		return bibtex
 	end
-
+	
+	# Handle special symbols
+	
+	def handle_special_symbols(references)
+		references.each do |reference|
+			handle_skands_for_reference_attribute_values(reference)
+		end
+	end
+	
+	def handle_special_symbols_for_reference_attribute_values(reference)
+		reference.reference_attributes.each do |ref_att|
+			special_symbols_for_string(ref_att.value)
+		end
+	end
+	
+	def special_symbols_for_string(s)
+		special_chars.each do |key, value|
+			s.tr(key, value)
+		end
+	end
+	
 end
