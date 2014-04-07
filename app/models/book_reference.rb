dde135ddc5eb73d62ac205558ca311d902ffac61
Kaltsoon <Kalle.ilves@helsinki.fi>
2014-04-07 10:51:06 +0300

    fix 3

--- app/models/book_reference.rb
+++ app/models/book_reference.rb
@@ -2,7 +2,7 @@ class BookReference
 	include ActiveModel::Model
 
 	def self.get_available_attributes
-		return ["title", "publisher", "year", "volume", "series", "address", "edition", "month", "note", "key"]
+		return ["author", "title", "publisher", "year", "volume", "series", "address", "edition", "month", "note", "key"]
 	end
 
 	def self.at_least_one
