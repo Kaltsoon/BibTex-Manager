d396534290bb45d1198b5a49ceea6a9d84fb6fc2
Kaltsoon <Kalle.ilves@helsinki.fi>
2014-04-18 18:57:19 +0300

    precompile

---
+++ public/assets/references/create_reference-d1a80e0c40f4914d8c433ca5c9838143.js
@@ -0,0 +1,54 @@
+var REFERENCE_CREATE_MODULE = (function(){
+
+	var display_fields = function(){
+		var type = $("#reference_ref_type").find(":selected").text();
+		if(type){
+			var actions = {
+				book: function(){
+					$("#attributes_form").html($("#book_attributes_form").html());
+				},
+				article: function(){
+					$("#attributes_form").html($("#article_attributes_form").html());
+				},
+				inproceedings: function(){
+					$("#attributes_form").html($("#inproceedings_attributes_form").html());
+				}
+			}
+			actions[type]();
+		}
+	}
+
+	var display_generated_id = function(){
+		var attributes = {}
+		$("#attributes_form input[type=text]").each(function(index, field){
+			var field_name = $(field).attr("name");
+			var attribute_name = field_name.substring(field_name.indexOf("[")+1, field_name.length-1)
+			attributes[attribute_name] = $(field).val();
+		});
+		fetch_generated_id(attributes);
+	}
+
+	var fetch_generated_id = function(attributes){
+		if(!$("#disable-generation").is(":checked")){
+			$.post("/generate_id_for_reference", { attributes: attributes }).done(function(data){
+				$("#reference_name").val(data.generated_id);
+			});
+		}
+	}
+
+	return {
+		display_fields: display_fields,
+		display_generated_id: display_generated_id
+	}
+})();
+
+$(document).on("page:load ready",function(){
+	REFERENCE_CREATE_MODULE.display_fields();
+	REFERENCE_CREATE_MODULE.display_generated_id();
+	$("#reference_ref_type").change(function(){
+		REFERENCE_CREATE_MODULE.display_fields();
+	});
+	$("#attributes_form input[type=text]").keyup(function(){
+		REFERENCE_CREATE_MODULE.display_generated_id();
+	});
+});
