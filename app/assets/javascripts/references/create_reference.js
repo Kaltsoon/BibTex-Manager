db6274b3c8d77ae6a6bb0ae79422d114a0040b54
Kaltsoon <Kalle.ilves@helsinki.fi>
2014-04-18 18:22:43 +0300

    log

--- app/assets/javascripts/references/create_reference.js
+++ app/assets/javascripts/references/create_reference.js
@@ -1,4 +1,5 @@
 var REFERENCE_CREATE_MODULE = (function(){
+
 	var display_fields = function(){
 		var type = $("#reference_ref_type").find(":selected").text();
 		if(type){
@@ -16,14 +17,33 @@ var REFERENCE_CREATE_MODULE = (function(){
 			actions[type]();
 		}
 	}
+
+	var display_generated_id = function(){
+		var attributes = {}
+		$("#attributes_form input[type=text]").each(function(index, field){
+			var field_name = $(field).attr("name");
+			var attribute_name = field_name.substring(field_name.indexOf("[")+1, field_name.length-1)
+			attributes[attribute_name] = $(field).val();
+		});
+	}
+
+	var fetch_generated_id = function(attributes){
+		
+	}
+
 	return {
-		display_fields: display_fields
+		display_fields: display_fields,
+		display_generated_id: display_generated_id
 	}
 })();
 
 $(document).on("page:load ready",function(){
 	REFERENCE_CREATE_MODULE.display_fields();
+	REFERENCE_CREATE_MODULE.display_generated_id();
 	$("#reference_ref_type").change(function(){
 		REFERENCE_CREATE_MODULE.display_fields();
+	});
+	$("#attributes_form input[type=text]").keyup(function(){
+		REFERENCE_CREATE_MODULE.display_generated_id();
 	});
 });
