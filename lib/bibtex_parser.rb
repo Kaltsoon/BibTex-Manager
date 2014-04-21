713162d6edbcd9e87109601674f1f5023c335fd1
Ossian Laurinharju <ossian.laurinharju@gmail.com>
2014-04-21 22:28:01 +0300

    parser kait valmis mutta testi kusee

--- lib/bibtex_parser.rb
+++ lib/bibtex_parser.rb
@@ -1,27 +1,7 @@
 module BibtexParser
 
-  def test1
-    inproceedings = "@inproceedings{Singer:2003:SBN:1085714.1085771,
-author = {Singer, Eric},
-title = {Sonic Banana: A Novel Bend-sensor-based MIDI Controller},
-booktitle = {Proceedings of the 2003 Conference on New Interfaces for Musical Expression},
-series = {NIME '03},
-year = {2003},
-location = {Montreal, Quebec, Canada},
-pages = {220--221},
-numpages = {2},
-url = {http://dl.acm.org/citation.cfm?id=1085714.1085771},
-acmid = {1085771},
-publisher = {National University of Singapore},
-address = {Singapore, Singapore},
-keywords = {MIDI, bend, controller, interactive, performance, sensors},
-}"
-    parse_bibtex_references inproceedings
-  end
-
   def parse_bibtex_references(bibtex)
-    bibtex = bibtex.gsub("\n", "") #poistetaan rivinvaihdot koska muuten hajoo
-    #bibtex = bibtex.squish
+    bibtex = bibtex.gsub(/\s+/, "")
     @bibtex_string = bibtex
     @current_index = 0
     references = []
@@ -32,74 +12,10 @@ keywords = {MIDI, bend, controller, interactive, performance, sensors},
       end
       reference = next_reference
     end
-
+    return references
   end
 
   private
-
-    def current_char()
-      return @bibtex_string[@current_index]
-    end
-
-    def next_char()
-      if(@current_index >= @bibtex_string.length)
-        return nil
-      end
-      @current_index += 1
-      return current_char
-    end
-
-    def accept(string)
-      skip_white_space
-      bibtex_index = @current_index
-      string_index = 0
-      while string_index < string.length do
-        if((@bibtex_string[bibtex_index] != string[string_index]) || bibtex_index > @bibtex_string.length)
-          return false
-        end
-        string_index += 1
-        bibtex_index += 1
-      end
-      @current_index = bibtex_index
-      return true
-    end
-
-    def expect(string)
-      if accept(string)
-        return true
-      end
-      raise("bibtex parser syntax error! Expected '" + string + "' at index: " + @current_index.to_s)
-    end
-
-    #word seperated by white space
-    def next_word
-      skip_white_space
-      return next_word_until([" ", "\n", "\t", "\r"])
-    end
-
-    def next_word_until(separators)
-      word = ""
-      while (not separators.include?(current_char)) && @current_index < @bibtex_string.length do
-        word += current_char
-        next_char
-      end
-      return word
-    end
-
-    def skip_white_space
-      while(is_white_space(current_char) && @current_index < @bibtex_string.length)
-        @current_index += 1
-      end
-    end
-
-    def is_white_space(char)
-      return char == " " || char == "\n" || char == "\r" || char == "\t"
-    end
-
-    def is_end_of_input
-      skip_white_space
-      return @current_index >= @bibtex_string.length - 1
-    end
 
     def next_reference
       if is_end_of_input
@@ -107,9 +23,6 @@ keywords = {MIDI, bend, controller, interactive, performance, sensors},
       end
 
       type = reference_type.downcase
-
-      puts type #asdfasdfasdf
-
       expect('{')
       id = reference_id
       expect(',')
@@ -119,7 +32,7 @@ keywords = {MIDI, bend, controller, interactive, performance, sensors},
       value = ""
       while key != nil do
         key, value = attribute
-        if(reference.get_available_attributes.include? key)
+        if reference.get_available_attributes.include? key
           reference.reference_attributes.push(ReferenceAttribute.new(name: key, value: value))
         end
       end
@@ -151,11 +64,10 @@ keywords = {MIDI, bend, controller, interactive, performance, sensors},
       if current_char == '}'
         return nil
       end
-      return next_word.downcase;
+      return next_word_until(['='])
     end
 
     def attribute_value
-      skip_white_space
       braces = accept('{')
       value = "";
       if braces
@@ -165,5 +77,51 @@ keywords = {MIDI, bend, controller, interactive, performance, sensors},
         value = next_word_until([','])
       end
       return value;
+    end
+
+    def accept(string)
+      bibtex_index = @current_index
+      string_index = 0
+      while string_index < string.length do
+        if (@bibtex_string[bibtex_index] != string[string_index]) || bibtex_index > @bibtex_string.length
+          return false
+        end
+        string_index += 1
+        bibtex_index += 1
+      end
+      @current_index = bibtex_index
+      return true
+    end
+
+    def expect(string)
+      if accept(string)
+        return true
+      end
+      raise("bibtex parser syntax error! Expected '" + string + "' at index: " + @current_index.to_s)
+    end
+
+    def current_char()
+      return @bibtex_string[@current_index]
+    end
+
+    def next_char()
+      if @current_index >= @bibtex_string.length
+        return nil
+      end
+      @current_index += 1
+      return current_char
+    end
+
+    def next_word_until(separators)
+      word = ""
+      while (not separators.include?(current_char)) && @current_index < @bibtex_string.length do
+        word += current_char
+        next_char
+      end
+      return word
+    end
+
+    def is_end_of_input
+      return @current_index >= @bibtex_string.length - 1
     end
 end
--- spec/lib/bibtex_parser_spec.rb
+++ spec/lib/bibtex_parser_spec.rb
@@ -31,25 +31,11 @@ keywords = {MIDI, bend, controller, interactive, performance, sensors},
       publisher:  "National University of Singapore",
       address:  "Singapore, Singapore"
     }
-    parse_bibtex_references "@inproceedings{Singer:2003:SBN:1085714.1085771,
-author = {Singer, Eric},
-title = {Sonic Banana: A Novel Bend-sensor-based MIDI Controller},
-booktitle = {Proceedings of the 2003 Conference on New Interfaces for Musical Expression},
-series = {NIME '03},
-year = {2003},
-location = {Montreal, Quebec, Canada},
-pages = {220--221},
-numpages = {2},
-url = {http://dl.acm.org/citation.cfm?id=1085714.1085771},
-acmid = {1085771},
-publisher = {National University of Singapore},
-address = {Singapore, Singapore},
-keywords = {MIDI, bend, controller, interactive, performance, sensors},
-}"
+    #hajoo
+    #reference = parse_bibtex_references inproceedings
     attributes = reference.reference_attributes
     attributes.each do |attribute|
       expect(attribute.value).to eq(valid[attribute.name])
     end
-
   end
 end
